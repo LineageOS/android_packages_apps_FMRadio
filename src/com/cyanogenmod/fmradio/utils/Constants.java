c00fb00c0fd2909e66993b3132277f42685b6611
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-03 11:34:29 +0530

    Merge branch 'jellybean' of git://github.com/freexperia/android_packages_apps_FMRadio.git
    Squashed commit of the following:
    
    commit 8590cd7d6ab668d1ff3f7fa9150cd98700497aa6
    Merge: 046d631 44e1957
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 23:30:05 2013 +0100
    
        Merge branch 'jellybean' of github.com:freexperia/android_packages_apps_FMRadio into jellybean
    
    commit 046d6318a762d2e53aebb9a10dd9adb153545932
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 23:28:48 2013 +0100
    
        Implement first time FM Band selection
    
    commit 44e19571b2038f4c462333a3bf04c9f926d7fa23
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 19:47:21 2013 +0100
    
        implement earplug start verification
    
        Signed-off-by: Pedro Veloso <pedro.n.veloso@gmail.com>
    
    commit 0af8bc425acd3840611c7c716991ab365acd29ee
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 19:25:31 2013 +0100
    
        better preferences handling. set preferences for future functionality
    
        Signed-off-by: Pedro Veloso <pedro.n.veloso@gmail.com>
    
    commit 00283853fec5c975e0a023fbea68449818279305
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 19:09:12 2013 +0100
    
        remove Stereo listener on destroy
    
        Signed-off-by: Pedro Veloso <pedro.n.veloso@gmail.com>
    
    commit d3ac23f55931deac1116d83740c5fc7d4d5824c2
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 19:05:27 2013 +0100
    
        update README
    
        Signed-off-by: Pedro Veloso <pedro.n.veloso@gmail.com>
    
    commit a2a55ac3c753dc56aba05a9bca35f8bbef17e9cc
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 18:48:58 2013 +0100
    
        conclude full RDS implementation according to RDS standards. update TODO
    
        Signed-off-by: Pedro Veloso <pedro.n.veloso@gmail.com>
    
    commit 44da0cf392be293785fd36916dee1e9778ae9c32
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 18:20:25 2013 +0100
    
        add Stereo/Mono display listener. some code cleanup
    
        Signed-off-by: Pedro Veloso <pedro.n.veloso@gmail.com>
    
    commit f9b18151eaf42ca5b69081f3e0bc92b3b20707f9
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 18:04:35 2013 +0100
    
        clear RDS text on frequency change
    
        Signed-off-by: Pedro Veloso <pedro.n.veloso@gmail.com>
    
    commit ab7b56845410a20e2689f0e2ba0d40f694e849c6
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 17:56:56 2013 +0100
    
        fix radio state when layout rotates
    
    commit 82ba3c53fc7d7515db698688e5df518c40fcf295
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 17:47:31 2013 +0100
    
        fix seek up, seek down and full scan behaviour
    
    commit 67b77dacbbacb4d435397867b2b110fcfc159127
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 16:09:25 2013 +0100
    
        centralized app preferences
    
    commit 43a9e56581fb7e3d29faf0efb4637c57c0ed428a
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sun Apr 14 15:59:40 2013 +0100
    
        small code enhancement and cleanup
    
    commit e8d4dcebd111562ab5726baed8707831d09a7eaf
    Author: Pedro Veloso <pedro.n.veloso@gmail.com>
    Date:   Sat Apr 13 21:04:19 2013 +0100
    
        more coherent version numbering on manifest
    
    Change-Id: I3eb9446b4233aae3c211923dd82db4fad6a9acc5

--- src/com/cyanogenmod/fmradio/utils/Constants.java
+++ src/com/cyanogenmod/fmradio/utils/Constants.java
@@ -13,4 +13,11 @@ public final class Constants {
     public static final int CHANNEL_OFFSET_50KHZ = 50;
 
     public static final String MEDIA_SOURCE = "fmradio://rx";
+    public static final String FM_RECEIVER_SERVICE = "fm_receiver";
+
+    // PREFERENCES
+    public static final String PREFS_SELECTED_BAND = "SELECTED_BAND";
+    public static final String PREFS_VERBOSE_LOGGING = "VERBOSE_LOGGING";
+    public static final String PREFS_NOTIFICATION_CONTROLS = "NOTIFICATION_CONTROLS";
+    public static final String PREFS_IS_FIRST_TIME = "IS_FIRST_TIME";
 }
