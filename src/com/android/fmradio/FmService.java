1f025e155a52862f48689cf76dceb4699b22d8d3
Ricardo Cerqueira <ricardo@cyngn.com>
2016-05-02 14:12:59 +0100

    FmService: Make usage of audiopatch a build-time option
    Change Ib732c8e8328035d2b3e60b7823446d1d75b3c107 assumes all devices
    have issues with direct volume configuration and require audiopatching
    to be disabled in favor of soft rendering. Don't do that, make it
    configurable.
    
    Change-Id: Ia334052043a292d37b44e727769232e55a673d35

--- src/com/android/fmradio/FmService.java
+++ src/com/android/fmradio/FmService.java
@@ -1683,32 +1683,34 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
 
     // Make sure patches count will not be 0
     private boolean isPatchMixerToEarphone(ArrayList<AudioPatch> patches) {
-    /*
         int deviceCount = 0;
         int deviceEarphoneCount = 0;
-        for (AudioPatch patch : patches) {
-            AudioPortConfig[] sources = patch.sources();
-            AudioPortConfig[] sinks = patch.sinks();
-            AudioPortConfig sourceConfig = sources[0];
-            AudioPortConfig sinkConfig = sinks[0];
-            AudioPort sourcePort = sourceConfig.port();
-            AudioPort sinkPort = sinkConfig.port();
-            Log.d(TAG, "isPatchMixerToEarphone " + sourcePort + " ====> " + sinkPort);
-            if (sourcePort instanceof AudioMixPort && sinkPort instanceof AudioDevicePort) {
-                deviceCount++;
-                int type = ((AudioDevicePort) sinkPort).type();
-                if (type == AudioSystem.DEVICE_OUT_WIRED_HEADSET ||
-                        type == AudioSystem.DEVICE_OUT_WIRED_HEADPHONE) {
-                    deviceEarphoneCount++;
+        if (mContext.getResources().getBoolean(R.bool.config_useSoftwareRenderingForAudio)) {
+            Log.w(TAG, "FIXME: forcing isPatchMixerToEarphone to return false. "
+                    + "Software rendering will be used.");
+            return false;
+        } else {
+            for (AudioPatch patch : patches) {
+                AudioPortConfig[] sources = patch.sources();
+                AudioPortConfig[] sinks = patch.sinks();
+                AudioPortConfig sourceConfig = sources[0];
+                AudioPortConfig sinkConfig = sinks[0];
+                AudioPort sourcePort = sourceConfig.port();
+                AudioPort sinkPort = sinkConfig.port();
+                Log.d(TAG, "isPatchMixerToEarphone " + sourcePort + " ====> " + sinkPort);
+                if (sourcePort instanceof AudioMixPort && sinkPort instanceof AudioDevicePort) {
+                    deviceCount++;
+                    int type = ((AudioDevicePort) sinkPort).type();
+                    if (type == AudioSystem.DEVICE_OUT_WIRED_HEADSET ||
+                            type == AudioSystem.DEVICE_OUT_WIRED_HEADPHONE) {
+                        deviceEarphoneCount++;
+                    }
                 }
             }
+            if (deviceEarphoneCount == 1 && deviceCount == deviceEarphoneCount) {
+                return true;
+            }
         }
-        if (deviceEarphoneCount == 1 && deviceCount == deviceEarphoneCount) {
-            return true;
-        }
-    */
-        Log.w(TAG, "FIXME: forcing isPatchMixerToEarphone to return false. "
-                 + "Software rendering will be used.");
         return false;
     }
 
