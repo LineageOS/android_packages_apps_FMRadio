b7745ce91bd1ca662e43fbcb514aa3c173dffcf4
Adrian DC <radian.dc@gmail.com>
2018-08-11 12:25:33 +0200

    FMRadio: Resolve android.support.v7.cardview rules
     * Automate inclusions with LOCAL_STATIC_ANDROID_LIBRARIES,
        resolving frameworks/support/v7/cardview/res path
    
    Change-Id: Ie1082eb81f287c4d352509c803612947786672cc

--- Android.mk
+++ Android.mk
@@ -36,10 +36,12 @@ LOCAL_JNI_SHARED_LIBRARIES := libfmjni
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-cardview
-LOCAL_RESOURCE_DIR = $(LOCAL_PATH)/res frameworks/support/v7/cardview/res
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v7-cardview \
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay --extra-packages android.support.v7.cardview
+LOCAL_RESOURCE_DIR = $(LOCAL_PATH)/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 include $(BUILD_PACKAGE)
 
