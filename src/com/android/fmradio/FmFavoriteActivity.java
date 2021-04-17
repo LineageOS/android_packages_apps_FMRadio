55b780992eb04ce26ab1a63cd9d059c39ef2d97d
Ash Blake <ash.blake@tutanota.com>
2021-04-17 20:35:33 +0200

    FMRadio: recognize MTK short antenna availability
    Change-Id: I8adc43a605b74800f9934e37d3c1e1fa825870a5

--- src/com/android/fmradio/FmFavoriteActivity.java
+++ src/com/android/fmradio/FmFavoriteActivity.java
@@ -33,6 +33,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.os.SystemProperties;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
@@ -513,7 +514,7 @@ public class FmFavoriteActivity extends Activity {
                     bundle = msg.getData();
                     boolean isHeadset = bundle.getBoolean(FmListener.KEY_IS_SWITCH_ANTENNA);
                     // if receive headset plugout, need set headset mode on ui
-                    if (!isHeadset) {
+                    if (!isHeadset && !SystemProperties.get("ro.vendor.mtk_fm_short_antenna_support").equals("1")) {
                         finish();
                     }
                     break;
