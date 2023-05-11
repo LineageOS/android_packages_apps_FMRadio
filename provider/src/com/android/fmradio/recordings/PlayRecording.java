b21f239f73337834d218457c377e09058cae0682
nift4 <nift4@protonmail.com>
2023-05-11 23:21:40 +0200

    FMRadio: fix listen to recordings
    FM app used to use file:// Uri to start a music player. However, in
    recent Android versions, this became a crashable offense
    (android.os.FileUriExposedException).
    
    To fix the crash, we can either:
    
    1. Disable StrictMode check -> won't help if music app cannot read video
       files (3gpp counts as video)
    2. Cleanly expose via FileProvider -> we can't, because uid=system
       cannot give out uri permissions for security reasons
    3. impersonate one of the system-whitelisted file providers (settings
       and tvsettings) -> HACK
    4. Implement stub app which hosts the FileProvider but isn't uid=system
    
    And that's why I implemented a tiny, no-UI stub app for making listen
    possible. To avoid security holes, the stub app is gated behind
    "signature" permission.
    
    Change-Id: Ibf3f3c06967dda025d489e6612678f38b549cbcb

---
+++ provider/src/com/android/fmradio/recordings/PlayRecording.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.fmradio.recordings;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+
+public class PlayRecording extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent oldIntent = getIntent();
+
+        if (oldIntent != null && oldIntent.hasExtra("type") && oldIntent.hasExtra("path")) {
+            Intent nextIntent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName(),
+                    new File(Uri.parse(oldIntent.getStringExtra("path")).getPath()));
+            nextIntent.setDataAndType(uri, oldIntent.getStringExtra("type"));
+            nextIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(nextIntent);
+        }
+
+        finish();
+    }
+}
