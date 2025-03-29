00763e019ba543c1e9e9048863dd452e65b5dda8
AlexandrKozlovskiy <k.sasha1994@gmail.com>
2025-03-29 13:48:18 +0300

    FmService: Fix loss of audio if we exit from main activity
    * Only can recreate when FM Radio is switched off.
    
    Recreation Steps:
    1. Enter the FM Radio app and toggle FM radio on.
    2. Press the toggle again to switch off FM radio.
    3. Press the back button on your device to close activity.
    4. Enjoy no sound until you reboot your device.
    
    Change-Id: I6457760a9425c568f6f5ce912a40029bd9fb630c

--- src/com/android/fmradio/FmService.java
+++ src/com/android/fmradio/FmService.java
@@ -1472,7 +1472,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
 
     @Override
     public void onDestroy() {
-        mAudioManager.setParameters("AudioFmPreStop=1");
+        mAudioManager.setParameters("AudioFmPreStop=0");
         setMute(true);
         // stop rds first, avoid blocking other native method
         if (isRdsSupported()) {
