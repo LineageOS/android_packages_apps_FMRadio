c8827be3ffe98d479d182131efe2ef1e867411a8
AlexandrKozlovskiy <k.sasha1994@gmail.com>
2025-03-29 13:48:18 +0300

    Fixed sound loosing,if we exit from main activity,when fm radio is switch off
    1. Enter in fm radio app and press button to switch on fm radio.
    2. Press this button again,to switch off fm radio.
    3. Press back button in your device,to close activity.
    4. Enjoy with no sound before rebooting of your device.
    
    This commit fix this behaviour.
    
    Change-Id: I6457760a9425c568f6f5ce912a40029bd9fb630c

--- src/com/android/fmradio/FmService.java
+++ src/com/android/fmradio/FmService.java
@@ -1473,7 +1473,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
 
     @Override
     public void onDestroy() {
-        mAudioManager.setParameters("AudioFmPreStop=1");
+        mAudioManager.setParameters("AudioFmPreStop=0");
         setMute(true);
         // stop rds first, avoid blocking other native method
         if (isRdsSupported()) {
