fc9eb94923262bba6c1ce0eb6ef13e3f1b2a10a5
AlexandrKozlovskiy <k.sasha1994@gmail.com>
2025-05-22 21:39:01 +0300

    Added base support of media buttons
    For now you can use media buttons on your headset to work with fm radio (recording of fm stations via media buttons yet not implemented).
    
    1. play/pause or stook buttons allow to pause/play fm radio
    2. previous/next track buttons,also as rewind with long press buttons allows to move between stations,find by autoscan .
    3. Rewind buttons allows to move between frequences.
    4. stopp button should stopp fm service.
    
    Change-Id: I4d86f3e9004d4669c1158f982d6f4564a11edb74

---
+++ src/com/android/fmradio/FMMediaButtonIntentReceiver.java
@@ -0,0 +1,20 @@
+package com.android.fmradio;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.view.KeyEvent;
+
+public class FMMediaButtonIntentReceiver extends BroadcastReceiver {
+    public interface MediaHandleListener {
+        public boolean handleIntent(Intent intent);
+    }
+    private MediaHandleListener listener;
+    public void setMediaHandleListener(MediaHandleListener listener) {
+        this.listener = listener;
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (listener != null) listener.handleIntent(intent);
+    }
+}
