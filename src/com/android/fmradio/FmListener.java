cba6b8449cdbdf0cdbf1bf7f67961c4f97a03424
AlexandrKozlovskiy <k.sasha1994@gmail.com>
2025-06-21 01:49:12 +0300

    Added support of recording from media buttons and other fixes and optimizations.
    * This huge commit allows to start/stop recording from media button. But it was required alot of rewrites in code.
    * Also this commit fix possible crash,which can happened,when we stop recording. And other optimizations and fixes,such as geting station info,fixing some typos in words,etc.
    
    Change-Id: Iae61a320d7c80c51cb5ac3d93fccbe55c8f8e043

--- src/com/android/fmradio/FmListener.java
+++ src/com/android/fmradio/FmListener.java
@@ -41,8 +41,14 @@ public interface FmListener {
     // FM record error occur
     int LISTEN_RECORDERROR = 0x00100110; // 1048848
 
+    /**
+        * record success start (it means,what
+        * mediamuxer was initialized successfully,so recording will be exactly started).
+        */
+    int LISTEN_RECORDSUCCESS = 0x00100111; // 1048849
+
     // FM record mode change
-    int LISTEN_RECORDMODE_CHANGED = 0x00100111; // 4018849
+    int LISTEN_RECORDMODE_CHANGED = 0x00100112; // 4018850
 
     // FM Record state changed
     int LISTEN_SPEAKER_MODE_CHANGED = 0x00101000; // 1052672
@@ -72,6 +78,8 @@ public interface FmListener {
     // For change speaker/earphone mode
     String KEY_IS_SPEAKER_MODE = "key_is_speaker_mode";
 
+    // Check,whether we should start recording after saving.
+    String START_RECORDING_AFTER_SAVING = "start_recording_after_saving";
     /**
      * handle message: call back from service to activity
      */
