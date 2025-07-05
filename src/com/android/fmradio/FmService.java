7e89b012877f7cea109eca09dc3edb4739ea577e
Cyber Knight <cyberknight755@gmail.com>
2025-07-05 18:59:11 +0800

    FmService: Set RECEIVER_NOT_EXPORTED
    Change-Id: I00627c63d1da2a9a91585e3ff5ae55d260076fce
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/com/android/fmradio/FmService.java
+++ src/com/android/fmradio/FmService.java
@@ -1361,7 +1361,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
         mMediaButtonIntentReceiver.setMediaHandleListener(listener);
         IntentFilter filter = new IntentFilter(Intent.ACTION_MEDIA_BUTTON);
         filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
-        registerReceiver(mMediaButtonIntentReceiver, filter);
+        registerReceiver(mMediaButtonIntentReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
         mMediaSession = new MediaSession(mContext, TAG);
         Intent i = new Intent(this, FMMediaButtonIntentReceiver.class)
                 .setFlags(Intent.FLAG_RECEIVER_FOREGROUND);
@@ -1581,7 +1581,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_HEADSET_PLUG);
         mBroadcastReceiver = new FmServiceBroadcastReceiver();
-        registerReceiver(mBroadcastReceiver, filter);
+        registerReceiver(mBroadcastReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
     }
 
     private void unregisterFmBroadcastReceiver() {
@@ -2042,7 +2042,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
         filter.addAction(Intent.ACTION_MEDIA_MOUNTED);
         filter.addAction(Intent.ACTION_MEDIA_UNMOUNTED);
         filter.addAction(Intent.ACTION_MEDIA_EJECT);
-        registerReceiver(mSdcardListener, filter);
+        registerReceiver(mSdcardListener, filter, Context.RECEIVER_NOT_EXPORTED);
     }
 
     private void unregisterSdcardListener() {
